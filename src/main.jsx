2ea61d55baf5bc142ce762775baa6eadc7c34248
ratu8ilqi5 <ratubilqis_pos@proton.me>
2026-08-01 19:00:14 +0700

    Add ErrorBoundary component to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,37 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true, error };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div style={{ padding: '20px', color: 'red', fontFamily: 'sans-serif' }}>
+          <h2>🚨 Waduh, ada Error JavaScript:</h2>
+          <pre style={{ background: '#eee', padding: '10px', borderRadius: '5px', overflowX: 'auto' }}>
+            {this.state.error?.toString()}
+          </pre>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ErrorBoundary>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ErrorBoundary>
   </React.StrictMode>,
 )
