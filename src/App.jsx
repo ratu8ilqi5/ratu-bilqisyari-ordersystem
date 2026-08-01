cc698c9b3359b772b53c456e896f7f701dc8b765
ratu8ilqi5 <ratubilqis_pos@proton.me>
2026-08-01 17:11:15 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import CustomerOrder from './CustomerOrder'
 import InvoiceView from './InvoiceView'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<CustomerOrder />} />
-        <Route path="/invoice/:token" element={<InvoiceView />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<CustomerOrder />} />
+      <Route path="/invoice/:token" element={<InvoiceView />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   )
 }
+
